4b19c6b69ed7367e7f2821479f89a375fdd11830
Shaun Hutch <shaunoff@Shauns-MBP.home>
2017-11-07 13:40:08 -0500

    complete

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const chalk = require('chalk')
 const app = express();
 const volleyball = require('volleyball')
 const nunjucks = require('nunjucks')
+var bodyParser = require('body-parser')
+var socketio = require('socket.io');
+// ...
 
 const port = 3000
-app.listen(port, ()=>{
+var server = app.listen(port, ()=>{
   console.log(`listening on port ${port}`)
 })
+var io = socketio.listen(server);
+
+
 app.set('view engine', 'html'); // have res.render work with html files
 
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
@@ -17,4 +23,6 @@ nunjucks.configure('views', {noCache: true});
 
 app.use(volleyball)
 app.use(express.static('public'))
-app.use('/', routes);
+app.use(bodyParser.urlencoded())
+app.use('/', routes(io));
+// parse application/x-www-form-urlencoded
--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,39 @@ const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
-router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
-});
 
-router.get('/users/:name', function(req, res) {
-  var splitName = req.params.name.split(' ');
-  var lowerName = splitName.map(function(name) {
-    name = name.toLowerCase()
-    name = name.charAt(0).toUpperCase() + name.slice(1)
-    return name;
-  })
-  var name = lowerName.join(' ');
-  // var name = req.params.name
-  var list = tweetBank.find( {name: name} );
-  res.render( 'index', { tweets: list } );
-});
 
-router.get('/tweets/:id', function (req, res) {
-  let tweetID = parseInt(req.params.id, 10)
-  let list = tweetBank.find({id: tweetID})
-  res.render( 'index', { tweets: list } );
-});
+module.exports = function (io) {
+  router.get('/', function (req, res) {
+    let tweets = tweetBank.list();
+    res.render( 'index', { tweets: tweets, showForm: true } );
+  });
 
-module.exports = router;
+  router.get('/users/:name', function(req, res) {
+    var splitName = req.params.name.split(' ');
+    var lowerName = splitName.map(function(name) {
+      name = name.toLowerCase()
+      name = name.charAt(0).toUpperCase() + name.slice(1)
+      return name;
+    })
+    var name = lowerName.join(' ');
+    // var name = req.params.name
+    var list = tweetBank.find( {name: name} );
+    res.render( 'index', { tweets: list, showForm: true, name: name} );
+  });
+
+  router.get('/tweets/:id', function (req, res) {
+    let tweetID = parseInt(req.params.id, 10)
+    let list = tweetBank.find({id: tweetID})
+    res.render( 'index', { tweets: list } );
+  });
+
+  router.post('/tweets', function(req, res) {
+    var name = req.body.name;
+    var text = req.body.text;
+    var newTweet = tweetBank.add(name, text);
+    io.sockets.emit('newTweet', {name, text});
+    res.redirect('/');
+  });
+  return router;
+};
